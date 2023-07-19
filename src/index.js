ed1eb7e86f3dcdb6a61d7ffbb04abf4a88d162c3
unknown <dipjal5678@gmai.com>
2023-07-19 14:46:25 +0545

    accomplished exercise 2.1

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./app";
+
+const el = ReactDOM.createRoot(document.getElementById("root"));
+
+el.render(<App />);
